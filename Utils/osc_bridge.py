294be75af9af82d2c18734b727bce1b82c962835
Rhapsodizer <alessandro.gorni@outlook.com>
2023-09-20 22:47:55 +0200

    Updated osc_bridge; chat works!

--- Utils/osc_bridge.py
+++ Utils/osc_bridge.py
@@ -1,5 +1,8 @@
 from pythonosc import udp_client
+import firebase_admin
+from firebase_admin import db
 
+# OSC definitions
 ip_addr = "127.0.0.1"
 port_SC = 57120 # towards supercollider
 port_LI = 12000 # towards layer interaction sketch
@@ -9,3 +12,31 @@ oscSC = udp_client.SimpleUDPClient(ip_addr, port_SC)
 oscPR = udp_client.SimpleUDPClient(ip_addr, port_LI)
 oscDM = udp_client.SimpleUDPClient(ip_addr, port_DM)
 oscCH = udp_client.SimpleUDPClient(ip_addr, port_CH)
+
+
+# Connect to firebase
+credential = firebase_admin.credentials.Certificate('H:/Documenti/POLIMI/2_1/CC/Project/client/certificate.json') # Change path
+default_app = firebase_admin.initialize_app(credential, {
+	'databaseURL': 'https://creativeproject-12185-default-rtdb.europe-west1.firebasedatabase.app'
+	})
+
+
+# Listeners
+def listenerU1(event):
+    oscCH.send_message("/username", [1, event.data])
+    print(event.data)
+
+def listenerC1(event):
+    oscCH.send_message("/char", [1, event.data])
+    print(event.data)
+
+def listenerU2(event):
+    oscCH.send_message("/username", [2, event.data])
+    
+def listenerC2(event):
+    oscCH.send_message("/char", [2, event.data])
+
+db.reference('table/username1').listen(listenerU1)
+db.reference('table/charStream1').listen(listenerC1)
+db.reference('table/username2').listen(listenerU2)
+db.reference('table/charStream2').listen(listenerC2)
