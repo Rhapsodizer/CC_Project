0f4ece83367ce513d692f50c1c8dc2f2142c5a16
Rhapsodizer <alessandro.gorni@outlook.com>
2023-09-26 15:57:55 +0200

    Added closing trigger

--- Utils/osc_bridge.py
+++ Utils/osc_bridge.py
@@ -10,7 +10,7 @@ port_LI = 12000  # towards layer interaction sketch
 port_DM = 12001  # towards drum machine
 port_CH = 12002  # towards melody chat
 oscSC = udp_client.SimpleUDPClient(ip_addr, port_SC)
-oscPR = udp_client.SimpleUDPClient(ip_addr, port_LI)
+oscLI = udp_client.SimpleUDPClient(ip_addr, port_LI)
 oscDM = udp_client.SimpleUDPClient(ip_addr, port_DM)
 oscCH = udp_client.SimpleUDPClient(ip_addr, port_CH)
 
